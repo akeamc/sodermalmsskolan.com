8a47969d0bfc97ba7d43240f015544cf45d1e5aa
Åke Amcoff <ake.amcoff@gmail.com>
2020-02-10 18:15:27 +0000

    Add QUIZLET! (basic, but it works)

--- src/components/quizlet/StudySetCard.tsx
+++ src/components/quizlet/StudySetCard.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { StudySet } from "../../utils/quizlet";
+import styles from "./StudySetCard.module.scss";
+import { H5 } from "../basic/Typography";
+import {Twemoji} from "react-emoji-render";
 
 export class StudySetCard extends React.Component<{ studySet: StudySet }> {
   render() {
     const { studySet } = this.props;
 
     return (
-      <div>
-        <pre>{studySet}</pre>
-      </div>
+      <a className={styles.studySetCard} href={studySet.url}>
+        <H5>{studySet.name}</H5>
+        <div className={styles.categories}>
+          {studySet.categories.map((category, index) => {
+            console.log(category);
+            return <Twemoji svg text={category} key={index} />
+          })}
+        </div>
+      </a>
     );
   }
 }
