bf59bcb1baa6bed9320e01969d26b17651579b45
Åke Amcoff <ake.amcoff@gmail.com>
2020-06-09 12:56:48 +0200

    Add GitHub CI

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,27 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: "eslint:recommended",
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es6: true,
+        jest: true,
+        node: true
     },
-    ecmaVersion: 2018,
-    sourceType: "module",
-  },
-  plugins: ["react", "jest"],
-  rules: {
-    indent: ["error", 4],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
-  },
+    extends: "eslint:recommended",
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: "module",
+    },
+    plugins: ["react", "jest"],
+    rules: {
+        indent: ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+    },
 };
