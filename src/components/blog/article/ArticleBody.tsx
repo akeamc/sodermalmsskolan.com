912e3115cd93c909c6c8bf6bb6b4ccfa79f0b81c
Åke Amcoff <ake.amcoff@gmail.com>
2020-06-10 16:37:48 +0200

    Add full support for articles (and they look good now)

--- src/components/blog/article/ArticleBody.tsx
+++ src/components/blog/article/ArticleBody.tsx
@@ -3,13 +3,6 @@ import { Post } from "../../../api/ghost/posts";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import ReactHtmlParser from "react-html-parser";
-
-function transform(node, index) {
-  if (node.type === "tag" && node.name === "div") {
-    return null;
-  }
-}
 
 const ArticleBody: React.FunctionComponent<{
   data: Post | null;
@@ -22,20 +15,17 @@ const ArticleBody: React.FunctionComponent<{
       <Container>
         <Row className="justify-content-center">
           <Col xs={12} md={10} lg={9} xl={8}>
-            <div className="article-body">
-              {loading ? (
-                <>
-                  <h1>{<Skeleton />}</h1>
-                  <p>{<Skeleton count={5} />}</p>
-                </>
-              ) : (
-                <div>
-                  {ReactHtmlParser(data.html, {
-                    transform,
-                  })}
-                </div>
-              )}
-            </div>
+            {loading ? (
+              <div className="article-body">
+                <h1>{<Skeleton />}</h1>
+                <p>{<Skeleton count={5} />}</p>
+              </div>
+            ) : (
+              <div
+                className="article-body"
+                dangerouslySetInnerHTML={{ __html: data.html }}
+              />
+            )}
           </Col>
         </Row>
       </Container>
