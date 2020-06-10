3426cdf953f615dc31e6c6533ee0405476456c0b
Åke Amcoff <ake.amcoff@gmail.com>
2020-06-10 12:24:28 +0200

    TODO: Add 404 handling for posts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,31 @@
 module.exports = {
-    env: {
-        browser: true,
-        es6: true,
-        jest: true,
-        node: true,
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: "eslint:recommended",
-    globals: {
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly",
-    },
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 2018,
-        sourceType: "module",
-    },
-    plugins: ["react", "jest"],
-    rules: {
-        indent: ["error", 4],
-        "linebreak-style": ["error", "unix"],
-        quotes: ["error", "double"],
-        semi: ["error", "always"],
-        "no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }]
-    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["react", "jest"],
+  rules: {
+    indent: ["error", 4],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "no-unused-vars": [
+      "error",
+      { vars: "all", args: "after-used", ignoreRestSiblings: false },
+    ],
+  },
 };
--- src/pages/blogg/[slug].tsx
+++ src/pages/blogg/[slug].tsx
@@ -13,52 +13,61 @@ import MetaSection from "../../components/blog/article/MetaSection";
 const Page: React.FunctionComponent = () => {
   const router = useRouter();
   const slug = (router.query.slug || "").toString();
-  const { data } = useSWR(`blog/posts/${slug}`, () => getPostBySlug(slug));
+  const { data, error } = useSWR(`blog/posts/${slug}`, () =>
+    getPostBySlug(slug)
+  );
   const loading = !data;
 
-  return (
-    <Layout>
-      <Header
-        fixedNav
-        backgroundImage={data?.feature_image}
-        style={{ minHeight: "50vh" }}
-      />
-      <section className="pt-8 pt-md-11">
-        <Container>
-          <Row className="justify-content-center">
-            <Col xs={12} md={10} lg={9} xl={8}>
-              <h1 className="display-4 text-center">
-                {loading ? <Skeleton /> : data.title}
-              </h1>
+  if (error) {
+    /**
+     * The post was probably not found.
+     */
+    alert("Post not found :(");
+  } else {
+    return (
+      <Layout>
+        <Header
+          fixedNav
+          backgroundImage={data?.feature_image}
+          style={{ minHeight: "50vh" }}
+        />
+        <section className="pt-8 pt-md-11">
+          <Container>
+            <Row className="justify-content-center">
+              <Col xs={12} md={10} lg={9} xl={8}>
+                <h1 className="display-4 text-center">
+                  {loading ? <Skeleton /> : data.title}
+                </h1>
 
-              <p className="lead mb-7 text-center text-muted">
-                {loading ? <Skeleton count={3} /> : data.custom_excerpt}
-              </p>
-              <MetaSection
-                loading={loading}
-                publishedAt={data?.published_at}
-                author={data?.primary_author}
-              />
-            </Col>
-          </Row>
-        </Container>
-      </section>
-      <ArticleBody loading={loading} data={data || null} />
-      <section className="pt-6 pt-md-8 pb-8 pb-md-11">
-        <Container>
-          <Row className="justify-content-center">
-            <Col xs={12} md={10} lg={9} xl={8}>
-              <MetaSection
-                loading={loading}
-                publishedAt={data?.published_at}
-                author={data?.primary_author}
-              />
-            </Col>
-          </Row>
-        </Container>
-      </section>
-    </Layout>
-  );
+                <p className="lead mb-7 text-center text-muted">
+                  {loading ? <Skeleton count={3} /> : data.custom_excerpt}
+                </p>
+                <MetaSection
+                  loading={loading}
+                  publishedAt={data?.published_at}
+                  author={data?.primary_author}
+                />
+              </Col>
+            </Row>
+          </Container>
+        </section>
+        <ArticleBody loading={loading} data={data || null} />
+        <section className="pt-6 pt-md-8 pb-8 pb-md-11">
+          <Container>
+            <Row className="justify-content-center">
+              <Col xs={12} md={10} lg={9} xl={8}>
+                <MetaSection
+                  loading={loading}
+                  publishedAt={data?.published_at}
+                  author={data?.primary_author}
+                />
+              </Col>
+            </Row>
+          </Container>
+        </section>
+      </Layout>
+    );
+  }
 };
 
 export default Page;
