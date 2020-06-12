ac1697db830f27b0754a28bd4485880590e40075
Åke Amcoff <ake.amcoff@gmail.com>
2020-06-12 17:23:22 +0200

    bruh

--- src/components/quizlet/StudySetGrid.tsx
+++ src/components/quizlet/StudySetGrid.tsx
@@ -14,17 +14,16 @@ export class StudySetGridItem extends React.Component<{
 }> {
   render() {
     const { studySet, loading = false } = this.props;
+    const author = new GenericUser({
+      name: studySet?.author?.username,
+      avatarUrl: studySet?.author?.avatarURL,
+      url: `https://quizlet.com/${studySet?.author?.username}`,
+    });
 
     return (
       <NarrowCard
         meta={{
-          authors: [
-            new GenericUser({
-              name: studySet.author.username,
-              avatarUrl: studySet.author.avatarURL,
-              url: `https://quizlet.com/${studySet.author.username}`,
-            }),
-          ],
+          authors: [author],
           date: studySet?.timestamp,
         }}
         href={studySet?.url}
