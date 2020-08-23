931738dd9f72b72f6e74cf627f88095a35dd20e6
Åke Amcoff <ake.amcoff@gmail.com>
2020-08-23 16:23:17 +0200

    Fix blog

--- src/pages/blogg/index.tsx
+++ src/pages/blogg/index.tsx
@@ -5,17 +5,18 @@ import { Section } from "../../components/layout/Section";
 import { Navigation } from "../../components/basic/Navigation";
 import FeaturedPost from "../../components/blog/FeaturedPost";
 import { Row } from "../../components/grid/Row";
+import { Hero } from "../../components/layout/Hero";
 
 export default class Page extends React.Component {
   render() {
     return (
       <Layout metadata={{ title: "Blogg" }}>
         <Navigation />
-        <Section>
+        <Hero>
           <Row>
             <FeaturedPost />
           </Row>
-        </Section>
+        </Hero>
         <Section>
           <PostGridAuto skip={1} />
         </Section>
