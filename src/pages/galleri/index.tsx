8a3e2e315ae5d863e34810b0f65b44b90ab09940
Åke Amcoff <ake.amcoff@gmail.com>
2020-08-30 21:33:32 +0200

    Add site metadata to gallery

--- src/pages/galleri/index.tsx
+++ src/pages/galleri/index.tsx
@@ -9,7 +9,18 @@ import { NormalWidth } from "../../components/grid/Col";
 
 const Page: React.FunctionComponent = () => {
   return (
-    <Layout metadata={{ title: "Galleri" }}>
+    <Layout
+      metadata={{
+        title: "Galleri",
+        description:
+          "Fotografier av maten som serveras på Södermalmsskolan av Sodexo.",
+        images: [
+          "https://cdn.discordapp.com/attachments/575993879837409290/576074256723476491/IMG_20190507_121005.jpg",
+          "https://cdn.discordapp.com/attachments/575993879837409290/622014849173553152/IMG_20190913_122411.jpg",
+          "https://cdn.discordapp.com/attachments/575993879837409290/666282862151991296/IMG_3695.JPG",
+        ],
+      }}
+    >
       <Navigation />
       <HeroWithTitle title="Galleri" />
       <Section>
