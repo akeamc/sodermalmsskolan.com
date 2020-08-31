e2edb819b8edac82daa8814183135c394ca7b0e2
Åke Amcoff <ake.amcoff@gmail.com>
2020-08-31 19:06:20 +0200

    Add class selection to schedule

--- src/components/schedule/MultiView.tsx
+++ src/components/schedule/MultiView.tsx
@@ -5,71 +5,74 @@ import { Row } from "../grid/Row";
 import { Col } from "../grid/Col";
 import { ScheduleViewer } from "./Schedule";
 import styled from "styled-components";
-import Select from "react-select";
 import createPersistedState from "use-persisted-state";
-const useScheduleFilterState = createPersistedState("schedule-filter");
+import { Select } from "../form/Select";
+const useScheduleClassState = createPersistedState("schedule-class");
 
-const FilterContainer = styled.div``;
+const FilterOptions = styled.div`
+  display: flex;
+  width: 100%;
+  margin: -1rem;
+`;
+
+const FilterOption = styled.div`
+  flex: 0 0 12rem;
+  margin: 1rem;
+`;
+
+const stringToSelectOption = (value: string) => {
+  return {
+    label: value,
+    value,
+  };
+};
 
 export const ScheduleMultiView: React.FunctionComponent<{
   schedules: Schedule[];
 }> = ({ schedules }) => {
-  const selectableGroups = CommonSchedule.selectableGroups;
+  const classes = schedules.map((schedule) => schedule.group);
 
-  const groupOption = (groupName: string) => {
-    return {
-      label: groupName,
-      value: groupName,
-    };
-  };
+  const classOptions = classes.map(stringToSelectOption);
 
-  const groupOptions = Array.from(selectableGroups).map(
-    ([category, groups]) => {
-      return {
-        label: category,
-        options: groups.map(groupOption),
-      };
-    }
+  const [selectedClass, setSelectedClass] = useScheduleClassState<string>(
+    classes[0]
   );
-
-  const [selectedGroups, setSelectedGroups] = useScheduleFilterState<string[]>(
-    Array.from(selectableGroups.values()).flat()
-  );
-
-  const defaultSelected = selectedGroups.map(groupOption);
 
   return (
     <>
       <Section>
         <Row>
           <Col>
-            <FilterContainer>
-              <Select
-                options={groupOptions}
-                isMulti
-                placeholder="Filtrera"
-                defaultValue={defaultSelected}
-                onChange={(selected: { label: string; value: string }[]) => {
-                  const groups = selected?.map((option) => option.value) || [];
-
-                  setSelectedGroups(groups);
-                }}
-              />
-            </FilterContainer>
+            <FilterOptions>
+              <FilterOption>
+                <Select
+                  options={classOptions}
+                  placeholder="Undervisningsgrupp"
+                  defaultValue={stringToSelectOption(selectedClass)}
+                  onChange={({ value }: { value: string; label: string }) => {
+                    setSelectedClass(value);
+                  }}
+                />
+              </FilterOption>
+            </FilterOptions>
           </Col>
         </Row>
       </Section>
-      {schedules.map((schedule, index) => {
-        return (
-          <Section key={index}>
-            <Row>
-              <Col>
-                <ScheduleViewer schedule={schedule} groups={selectedGroups} />
-              </Col>
-            </Row>
-          </Section>
-        );
-      })}
+      {schedules.reduce((schedules, schedule, index) => {
+        if (schedule.group === selectedClass) {
+          schedules.push(
+            <Section key={index}>
+              <Row>
+                <Col>
+                  <ScheduleViewer schedule={schedule} />
+                </Col>
+              </Row>
+            </Section>
+          );
+        }
+
+        return schedules;
+      }, [])}
     </>
   );
 };
