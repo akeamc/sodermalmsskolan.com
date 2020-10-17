cde9e23aee798d6d6e48fc86e98d8745fe68d0b8
ThePicoNerd <ake.amcoff@lynx.agency>
2020-10-17 21:42:52 +0200

    thanks next.js

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import {
+import Document, {
   Html,
   Head,
   Main,
@@ -8,54 +8,54 @@ import {
 } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 import React from "react";
-import { NextPage } from "next";
 
 interface Props extends DocumentInitialProps {
   styles: JSX.Element;
 }
 
-const Document: NextPage<Props> = ({ styles }) => {
-  return (
-    <Html lang="sv">
-      <Head>
-        <link href="https://rsms.me/inter/inter.css" rel="stylesheet" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,900&display=swap"
-          rel="stylesheet"
-        ></link>
-        {styles}
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-};
+export default class MyDocument extends Document<Props> {
+  static async getInitialProps(ctx: DocumentContext): Promise<Props> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-Document.getInitialProps = async (ctx: DocumentContext) => {
-  const sheet = new ServerStyleSheet();
-  const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
 
-  try {
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
-      });
-
-    const initialProps = await Document.getInitialProps(ctx);
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          {sheet.getStyleElement()}
-        </>
-      ),
-    };
-  } finally {
-    sheet.seal();
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
-};
 
-export default Document;
+  render(): JSX.Element {
+    return (
+      <Html lang="sv">
+        <Head>
+          <link href="https://rsms.me/inter/inter.css" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,900&display=swap"
+            rel="stylesheet"
+          ></link>
+          {this.props.styles}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
