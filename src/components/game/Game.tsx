fb4485f9902e7177c7df76599856f12f782c872a
ThePicoNerd <ake.amcoff@lynx.agency>
2020-10-18 15:26:14 +0200

    remove unused import

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import { CanvasRenderFunction } from "../../hooks/canvas";
 import { useKeyEvents } from "../../hooks/key";
 import Canvas from "../basic/Canvas";
