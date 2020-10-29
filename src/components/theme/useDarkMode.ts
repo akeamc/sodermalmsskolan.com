3dd4f90715b6dc41d594cf42e522b0540514893e
ThePicoNerd <ake.amcoff@lynx.agency>
2020-10-29 19:22:51 +0100

    Make theme toggler look slightly better

--- src/components/theme/useDarkMode.ts
+++ src/components/theme/useDarkMode.ts
@@ -1,6 +1,7 @@
-import { StateSetter, usePersistedState } from "../../hooks/persistedstate";
+import { Dispatch, SetStateAction } from "react";
+import { usePersistedState } from "../../hooks/persistedstate";
 
-export const useDarkMode = (): [boolean, StateSetter<boolean>] => {
+export const useDarkMode = (): [boolean, Dispatch<SetStateAction<boolean>>] => {
   const [darkMode, setDarkMode] = usePersistedState<boolean>(
     "dark-mode",
     false
