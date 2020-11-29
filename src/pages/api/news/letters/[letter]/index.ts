5fd254bcba912b149f08e24b41d9b6ceab1388c0
ThePicoNerd <ake.amcoff@lynx.agency>
2020-11-29 21:22:37 +0100

    bruh

--- src/pages/api/news/letters/[letter]/index.ts
+++ src/pages/api/news/letters/[letter]/index.ts
@@ -3,10 +3,14 @@ import withAuth from "../../../../../lib/auth/withAuth";
 import { ServerLetter } from "../../../../../lib/news/structures/server/Letter";
 import { LetterStatic } from "../../../../../lib/news/structures/shared/Letter";
 
-const handler: NextApiHandler<LetterStatic> = async (req, res) => {
+const handler: NextApiHandler<LetterStatic | string> = async (req, res) => {
   const id = req.query.letter?.toString();
 
-  const letter = await ServerLetter.fetch(id);
+  const letter = await ServerLetter.fetch(id).catch(() => null);
+
+  if (!letter) {
+    return res.status(404).send(`letter with id \`${id}\` not found`);
+  }
 
   return res.json(letter.serialize());
 };
