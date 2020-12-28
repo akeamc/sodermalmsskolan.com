d2c19fbaec73e452c474f852df766c36b356b631
ThePicoNerd <ake.amcoff@lynx.agency>
2020-12-28 21:50:41 +0100

    refactor calendar tabs

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -13,10 +13,6 @@ import CalendarWeek from "./Week";
 import usePlaceholderEvents from "../../lib/calendar/hooks/usePlaceholderEvents";
 import ScheduledCalendarEvent from "../../lib/calendar/event/ScheduledCalendarEvent";
 import CalendarEventInstance from "../../lib/calendar/event/CalendarEventInstance";
-import SegmentedControl from "../form/SegmentedControl";
-import { FormOption } from "../form/types";
-import useLocale from "../../hooks/useLocale";
-import capitalize from "../../lib/utils/capitalize";
 
 dayjs.extend(isoWeek);
 
@@ -41,8 +37,6 @@ const Calendar: FunctionComponent<CalendarProps> = ({
   const [evaluatedEvents, setEvaluatedEvents] = useState<CalendarEventInstance[]>(null);
 
   const [cursor] = useState<Dayjs>(dayjs());
-
-  const { language } = useLocale();
 
   const dayCount = hideWeekend ? 5 : 7;
 
@@ -91,37 +85,8 @@ const Calendar: FunctionComponent<CalendarProps> = ({
   const rowPadEnd = shrink ? (86400 / rowDuration) - Math.ceil(latest / rowDuration) : 0;
   const rows = 86400 / rowDuration - rowPadStart - rowPadEnd;
 
-  const weekdayControlOptions: FormOption[] = useMemo(() => {
-    const options: FormOption[] = Array.from({
-      length: dayCount,
-    }).map((_, index) => ({
-      value: index.toString(),
-      label: capitalize(dayjs().locale(language).isoWeekday(index + 1).format("dddd")),
-    }));
-
-    return options;
-  }, [dayCount, language]);
-
-  const [activeTab, setActiveTab] = useState<string>(() => {
-    const weekday = (dayjs().isoWeekday() + 6) % 7;
-
-    return Math.min(weekday, dayCount - 1).toString();
-  });
-
   return (
     <div>
-      <div css={{
-        [media(breakpoints.large)]: {
-          display: "none",
-        },
-      }}
-      >
-        <SegmentedControl
-          options={weekdayControlOptions}
-          onChange={setActiveTab}
-          value={activeTab}
-        />
-      </div>
       <div css={{
         position: "relative",
         "--row-duration": rowDuration,
@@ -143,22 +108,11 @@ const Calendar: FunctionComponent<CalendarProps> = ({
           rowPadStart={rowPadStart}
           rowDuration={rowDuration}
         />
-        <div css={{
-          marginLeft: "var(--labels-width)",
-
-          [media(breakpoints.large)]: {
-            display: "flex",
-            height: "calc(var(--rows) * var(--row-height)) + var(--header-height)",
-          },
-        }}
-        >
-          <CalendarWeek
-            placeholder={!events}
-            dayCount={dayCount}
-            eventInstances={eventInstances}
-            activeTab={parseInt(activeTab, 10)}
-          />
-        </div>
+        <CalendarWeek
+          placeholder={!events}
+          dayCount={dayCount}
+          eventInstances={eventInstances}
+        />
       </div>
     </div>
   );
