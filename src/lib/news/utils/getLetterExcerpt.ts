14bc4915708abcaac33468ad596bc7408f45e1a8
ThePicoNerd <ake.amcoff@lynx.agency>
2021-01-07 22:11:53 +0100

    add unit tests, fix regex

--- src/lib/news/utils/getLetterExcerpt.ts
+++ src/lib/news/utils/getLetterExcerpt.ts
@@ -7,7 +7,7 @@
  * @returns {string} A trimmed, clean version of the content.
  */
 const getLetterExcerpt = (content = ""): string => {
-  const regex = /Veckobrev [a-z]+ v.( ?)[0-9]+/gi;
+  const regex = /Veckobrev\s[a-z]+\sv\.?\s?[0-9]+/gi;
 
   const matchIndex = Math.max(content.search(regex), 0);
   const matchLength = content.match(regex)?.[0]?.length || 0;
