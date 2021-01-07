797d369d553e9dea0a9e82c534cc8043d7af64f0
ThePicoNerd <ake.amcoff@lynx.agency>
2021-01-07 23:03:39 +0100

    Refactor study sets

--- src/lib/quizlet/structures/shared/StudySet.ts
+++ src/lib/quizlet/structures/shared/StudySet.ts
@@ -26,25 +26,6 @@ export class StudySet implements Serializable<StudySetStatic> {
     return `https://quizlet.com/${this.id}`;
   }
 
-  public static get idRegExp(): RegExp {
-    return /[0-9]{9}/;
-  }
-
-  public static get pathRegExp(): RegExp {
-    return new RegExp(
-      /^\//.source + this.idRegExp.source + /\/([a-z0-9-]+\/)?$/.source,
-    );
-  }
-
-  /**
-   * Extract the id from a Quizlet study set url.
-   *
-   * @param url
-   */
-  public static parseUrl(url: string): string {
-    return url.match(/[0-9]{9}/)[0];
-  }
-
   public serialize(): StudySetStatic {
     return {
       id: this.id,
