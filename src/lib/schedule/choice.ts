eb1821fb52f782e4aeb5e5ac176c975dff12a1af
Åke Amcoff <ake.amcoff@lynx.agency>
2021-01-11 08:41:38 +0100

    Fix o93 schedule partially

--- src/lib/schedule/choice.ts
+++ src/lib/schedule/choice.ts
@@ -48,15 +48,17 @@ export const CHOICES: PeriodChoice[] = [
         new Period(0, 9, 45, 75, SUBJECTS.MATH, "A311"),
         new Period(0, 11, 10, 75, SUBJECTS.SOCIAL_STUDIES, "A309"),
         new Period(0, 13, 15, 70, SUBJECTS.SWEDISH, "A308"),
-        new Period(1, 8, 25, 65, SUBJECTS.MATH, "A307"),
+        new Period(1, 8, 25, 65, SUBJECTS.MATH, "A311"),
         new Period(1, 9, 50, 50, SUBJECTS.SPORTS, "B501"),
         new Period(1, 13, 10, 65, SUBJECTS.SWEDISH, "A308"),
-        new Period(2, 8, 20, 55, SUBJECTS.SCIENCE, "A311"),
+        new Period(2, 8, 20, 55, SUBJECTS.SCIENCE, "A415"),
+        new Period(2, 9, 25, 50, SUBJECTS.MATH, "A311"),
+        new Period(2, 10, 20, 50, SUBJECTS.SWEDISH, "A308"),
         new Period(2, 11, 20, 65, SUBJECTS.SOCIAL_STUDIES, "A309"),
         new Period(2, 13, 15, 50, SUBJECTS.ENGLISH, "A307"),
-        new Period(3, 11, 30, 55, SUBJECTS.MATH, "A307"),
+        new Period(3, 11, 25, 55, SUBJECTS.MATH, "A307"),
         new Period(3, 13, 5, 50, SUBJECTS.SOCIAL_STUDIES, "A309"),
-        new Period(4, 14, 40, 50, SUBJECTS.SCIENCE, "A311"),
+        new Period(4, 14, 55, 50, SUBJECTS.SCIENCE, "A415"),
       ]),
     ],
   }, {
@@ -82,23 +84,23 @@ export const CHOICES: PeriodChoice[] = [
     collections: [
       new PeriodCollection("Tyska", "TY", [
         new Period(1, 14, 25, 75, SUBJECTS.GERMAN, "A310"),
-        new Period(2, 14, 25, 75, SUBJECTS.GERMAN, "A309"),
+        new Period(4, 10, 55, 75, SUBJECTS.GERMAN, "A310"),
       ]),
       new PeriodCollection("Franska", "FR", [
         new Period(1, 14, 25, 75, SUBJECTS.FRENCH, "A221"),
-        new Period(2, 14, 25, 75, SUBJECTS.FRENCH, "A221"),
+        new Period(4, 10, 55, 75, SUBJECTS.FRENCH, "A221"),
       ]),
       new PeriodCollection("Spanska (AAV)", "SP-A", [
         new Period(1, 14, 25, 75, SUBJECTS.SPANISH, "A110"),
-        new Period(2, 14, 25, 75, SUBJECTS.SPANISH, "A110"),
+        new Period(4, 10, 55, 75, SUBJECTS.SPANISH, "A110"),
       ]),
       new PeriodCollection("Spanska (CTH)", "SP-C", [
         new Period(1, 14, 25, 75, SUBJECTS.SPANISH, "A220"),
-        new Period(2, 14, 25, 75, SUBJECTS.SPANISH, "A220"),
+        new Period(4, 10, 55, 75, SUBJECTS.SPANISH, "A220"),
       ]),
       new PeriodCollection("ASVEN", "ASV", [
         new Period(1, 14, 25, 75, SUBJECTS.SWEDISH, "A308"),
-        new Period(2, 14, 25, 75, SUBJECTS.SWEDISH, "A308"),
+        new Period(4, 10, 55, 75, SUBJECTS.SWEDISH, "A308"),
       ]),
     ],
   }, {
