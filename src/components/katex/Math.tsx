f386a978a6ec0a4d791d0c0a0fb3a817569a777c
ThePicoNerd <ake.amcoff@lynx.agency>
2021-01-29 10:19:48 +0100

    math docs

--- src/components/katex/Math.tsx
+++ src/components/katex/Math.tsx
@@ -4,7 +4,19 @@ import katex from "katex";
 import "katex/dist/contrib/mhchem.js";
 import "katex/dist/katex.min.css";
 
-const Math: FunctionComponent<{math: string, display?: boolean}> = ({ math, display = false }) => {
+export interface MathProps {
+  math: string;
+  display?: boolean;
+}
+
+/**
+ * A component used to render KaTeX to html.
+ *
+ * @param {React.PropsWithChildren<MathProps>} props Props.
+ *
+ * @returns {React.ReactElement} The rendered math.
+ */
+const Math: FunctionComponent<MathProps> = ({ math, display = false }) => {
   const html = katex.renderToString(math, {
     displayMode: display,
     output: "html",
