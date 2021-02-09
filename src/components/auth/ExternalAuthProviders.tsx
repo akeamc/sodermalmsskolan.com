9b96303b9a552697344e64ea9657b341c959b2fc
ThePicoNerd <ake.amcoff@lynx.agency>
2021-02-09 21:27:55 +0100

    switch to firebase/app

--- src/components/auth/ExternalAuthProviders.tsx
+++ src/components/auth/ExternalAuthProviders.tsx
@@ -1,4 +1,4 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
 import React, { FunctionComponent } from "react";
 import reportEvent from "../../lib/analytics/reportEvent";
 import { auth } from "../../lib/firebase/firebase";
