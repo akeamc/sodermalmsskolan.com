e26abf9b063f96bfa34f8637a59a54b0b4b6ba7d
ThePicoNerd <ake.amcoff@lynx.agency>
2021-02-24 15:20:47 +0100

    Add KaTeX unit tests

--- src/components/post/RichText.test.tsx
+++ src/components/post/RichText.test.tsx
@@ -1,5 +1,6 @@
-import { render } from "@testing-library/react";
+import { act, render } from "@testing-library/react";
 import React from "react";
+import preloadAll from "jest-next-dynamic";
 import RichText from "./RichText";
 
 const html = `
@@ -9,6 +10,9 @@ const html = `
   How does it handle
   multiple
   lines?
+</p>
+<p>
+  $$\\frac{1}{x}$$
 </p>
 <script>
 alert("oof");
@@ -43,9 +47,15 @@ alert("oof");
 <blockquote>deep quote</blockquote>
 `;
 
+beforeAll(async () => {
+  await preloadAll();
+});
+
 describe("<RichText /> test", () => {
-  it("renders as usual", () => {
+  it("renders as usual", async () => {
     const result = render(<RichText html={html} />);
+
+    await act(async () => {});
 
     expect(result.asFragment()).toMatchSnapshot();
   });
