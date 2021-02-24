6c3420745bf7cd6b5add7ac249126b50094119fb
ThePicoNerd <ake.amcoff@lynx.agency>
2021-02-24 15:22:22 +0100

    coverage

--- src/components/typography/getTextSize.test.ts
+++ src/components/typography/getTextSize.test.ts
@@ -1,9 +1,13 @@
-import textSize from "./getTextSize";
+import getTextSize from "./getTextSize";
 
 describe("text size test", () => {
   it("returns the expected sizes", () => {
-    expect(textSize(1)).toBe(12);
-    expect(textSize(6)).toBe(24);
-    expect(textSize(17)).toBe(92);
+    expect(getTextSize(1)).toBe(12);
+    expect(getTextSize(6)).toBe(24);
+    expect(getTextSize(17)).toBe(92);
+  });
+
+  it("throws if invalid integers are provided", () => {
+    expect(() => getTextSize(0)).toThrow();
   });
 });
