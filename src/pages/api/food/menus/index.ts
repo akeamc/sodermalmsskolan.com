add5a60a8612b7db34aa06355cfc15081d5f6226
ThePicoNerd <ake.amcoff@lynx.agency>
2021-03-22 15:20:35 +0100

    Cache header modernization

--- src/pages/api/food/menus/index.ts
+++ src/pages/api/food/menus/index.ts
@@ -1,11 +1,23 @@
 import { NextApiHandler } from "next";
 import ServerMenu from "../../../../lib/food/structures/server/Menu";
 import { IMenu } from "../../../../lib/food/structures/shared/Menu";
+import { getCacheHeader } from "../../../../lib/utils/cache";
 
+/**
+ * Menu API handler.
+ *
+ * @param {import("next").NextApiRequest} _ Request.
+ * @param {import("next").NextApiResponse} res Response.
+ *
+ * @returns {void}
+ */
 const handler: NextApiHandler<IMenu[]> = async (_, res) => {
   const menus = await ServerMenu.fetchAll();
 
-  res.setHeader("Cache-Control", "s-maxage=86400");
+  res.setHeader("Cache-Control", getCacheHeader({
+    maxAge: 3600,
+    staleWhileRevalidate: 86400,
+  }));
 
   return res.json(menus.map((menu) => menu.serialize()));
 };
