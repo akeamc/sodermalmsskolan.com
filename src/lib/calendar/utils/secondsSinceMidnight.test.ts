10219d2f9d5df302a93282612c89e9397210d02c
ThePicoNerd <ake.amcoff@lynx.agency>
2021-04-02 12:34:24 +0200

    fix unit tests

--- src/lib/calendar/utils/secondsSinceMidnight.test.ts
+++ src/lib/calendar/utils/secondsSinceMidnight.test.ts
@@ -2,6 +2,6 @@ import { DateTime } from "luxon";
 import secondsSinceMidnight from "./secondsSinceMidnight";
 
 test("seconds since midnight", () => {
-  expect(secondsSinceMidnight(DateTime.fromISO("2020-04-20T16:20:00Z"))).toBe(16 * 3600 + 20 * 60);
-  expect(secondsSinceMidnight(DateTime.fromISO("1970-01-01T00:00:01Z"))).toBe(1);
+  expect(secondsSinceMidnight(DateTime.fromISO("2020-04-20T16:20:00"))).toBe(16 * 3600 + 20 * 60);
+  expect(secondsSinceMidnight(DateTime.fromISO("1970-01-01T00:00:01"))).toBe(1);
 });
