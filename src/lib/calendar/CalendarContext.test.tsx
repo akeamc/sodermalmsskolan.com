185763d1c3b384aa809a3e5a7c894b53caf2c554
ThePicoNerd <ake.amcoff@lynx.agency>
2021-04-08 10:37:18 +0200

    coverage

--- src/lib/calendar/CalendarContext.test.tsx
+++ src/lib/calendar/CalendarContext.test.tsx
@@ -1,7 +1,8 @@
 import { render } from "@testing-library/react";
 import { isFunction } from "lodash";
+import { DateTime } from "luxon";
 import React from "react";
-import CalendarContext from "./CalendarContext";
+import CalendarContext, { CalendarContextProvider } from "./CalendarContext";
 
 describe("calendar context tests", () => {
   test("default functions should throw", () => {
@@ -18,5 +19,15 @@ describe("calendar context tests", () => {
         }}
       </CalendarContext.Consumer>,
     );
+  });
+
+  test("the default `initialCursor` should be equal to `DateTime.now()`", () => {
+    const spy = jest.spyOn(DateTime, "now");
+
+    render(
+      <CalendarContextProvider />,
+    );
+
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 });
