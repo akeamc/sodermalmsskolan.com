81aae1c0f337cb4b33a33d74b4930bd9a211257c
ThePicoNerd <ake.amcoff@lynx.agency>
2021-04-12 17:42:27 +0200

    css

--- src/components/calendar/CalendarEventDisplay/index.tsx
+++ src/components/calendar/CalendarEventDisplay/index.tsx
@@ -1,14 +1,24 @@
+import classNames from "classnames/bind";
 import { transparentize } from "polished";
-import React, { FunctionComponent } from "react";
+import React, { CSSProperties, FunctionComponent } from "react";
 import CalendarEventInstance from "../../../lib/calendar/event/CalendarEventInstance";
 import { getHumanReadableDuration } from "../../../lib/calendar/utils/humanReadable";
 import secondsSinceMidnight from "../../../lib/calendar/utils/secondsSinceMidnight";
-import { breakpoints, media } from "../../../styles/breakpoints";
 import relativelyReadableColor from "../../../styles/utils/relativelyReadableColor";
-import Time from "../../typography/atomics/Time";
+import styles from "./index.module.scss";
+
+const cx = classNames.bind(styles);
 
 export interface CalendarEventDisplayProps {
   calendarEvent: CalendarEventInstance;
+}
+
+export interface CSSVariables extends CSSProperties {
+  "--start": number;
+  "--duration": number;
+  "--color": string;
+  "--text-color": string;
+  "--dark-color": string;
 }
 
 /**
@@ -35,94 +45,36 @@ const CalendarEventDisplay: FunctionComponent<CalendarEventDisplayProps> = ({
   const endInSeconds = startInSeconds + duration;
   const compact = duration <= 1800;
 
+  const variables: CSSVariables = {
+    "--start": startInSeconds,
+    "--duration": duration,
+    "--color": color,
+    "--text-color": relativelyReadableColor(color),
+    "--dark-color": transparentize(0.8, color),
+  };
+
   return (
-    <div css={{
-      position: "absolute",
-      top: `calc(var(--hour-height) * ${startInSeconds} / 3600)`,
-      borderTop: `1px solid ${color}`,
-      boxSizing: "border-box",
-      height: `calc(var(--hour-height) * ${duration} / 3600)`,
-      width: "100%",
-      paddingTop: "var(--cell-spacing)",
-    }}
+    <div
+      className={cx("container")}
+      style={variables}
     >
-      <div css={[{
-        height: "100%",
-        width: "100%",
-        backgroundColor: color,
-        color: relativelyReadableColor(color),
-        padding: compact ? 6 : 8,
-        boxSizing: "border-box",
-        borderRadius: 6,
-        wordBreak: "break-word",
-
-        "@media (prefers-color-scheme: dark)": {
-          backgroundColor: transparentize(0.8, color),
-          color: "#fff",
-        },
-      }, compact ? {
-        display: "flex",
-        flexDirection: "row-reverse",
-        justifyContent: "space-between",
-        alignItems: "center",
-      } : {
-        [media(breakpoints.extraLarge)]: {
-          padding: 12,
-        },
-      }]}
+      <div className={cx("calendar-event", {
+        compact,
+      })}
       >
-        <span css={{
-          margin: 0,
-          fontWeight: 500,
-          fontSize: 12,
-          letterSpacing: "-0.022em",
-          opacity: 0.5,
-          lineHeight: 1.1,
-          display: "inline-block",
-
-          [media(breakpoints.extraLarge)]: {
-            fontSize: 14,
-          },
-        }}
-        >
-          <Time>
+        <span className={cx("meta")}>
+          <time>
             {getHumanReadableDuration(startInSeconds)}
-            <span css={{
-              display: "none",
-
-              [media(breakpoints.medium)]: {
-                display: "inline",
-              },
-            }}
-            >
+            <span className={cx("end")}>
               –
               {getHumanReadableDuration(endInSeconds)}
             </span>
-          </Time>
+          </time>
           {location ? (
-            <>
-              {" · "}
-              <span>{location}</span>
-            </>
+            <span>{location}</span>
           ) : undefined}
         </span>
-        <h2 css={{
-          margin: 0,
-          marginTop: compact ? undefined : "0.5em",
-          marginRight: compact ? 4 : undefined,
-          fontWeight: 500,
-          fontSize: 12,
-          letterSpacing: "-0.022em",
-
-          [media(breakpoints.large)]: {
-            fontSize: 16,
-          },
-
-          [media(breakpoints.extraLarge)]: {
-            fontSize: 18,
-          },
-        }}
-        >
+        <h2>
           {summary}
         </h2>
       </div>
