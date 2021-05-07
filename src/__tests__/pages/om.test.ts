94f0e6e1d61a29cfd406267b89c73112c8596172
ThePicoNerd <ake.amcoff@lynx.agency>
2021-05-07 11:36:27 +0200

    log

--- src/__tests__/pages/om.test.ts
+++ src/__tests__/pages/om.test.ts
@@ -4,11 +4,13 @@ import { screen } from "@testing-library/react";
 describe("about us test", () => {
   jest.unmock("next/head");
 
+  console.log(process.cwd());
+
   it("should render correctly", async () => {
     jest.setTimeout(10000);
 
     const { render } = await getPage({
-      route: "/om/",
+      route: "/om",
     });
 
     render();
