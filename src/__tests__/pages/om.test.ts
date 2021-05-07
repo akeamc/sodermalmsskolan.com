b4dc5c6d0892b28a422cceb3925f7147cb6ee9a5
ThePicoNerd <ake.amcoff@lynx.agency>
2021-05-07 11:41:28 +0200

    add log with explanation

--- src/__tests__/pages/om.test.ts
+++ src/__tests__/pages/om.test.ts
@@ -5,6 +5,11 @@ describe("about us test", () => {
   jest.unmock("next/head");
 
   it("should render correctly", async () => {
+    // For some reason, the test fails on GitHub Actions if this log statement is missing.
+    // I just want my tests to pass.
+    // eslint-disable-next-line no-console
+    console.log(process.cwd());
+
     const { render } = await getPage({
       route: "/om",
     });
