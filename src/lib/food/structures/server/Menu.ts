836ba94e414cba648627f1e3ea22e4076b796b13
ThePicoNerd <ake.amcoff@lynx.agency>
2021-05-17 16:06:43 +0200

    hotfix: menus

--- src/lib/food/structures/server/Menu.ts
+++ src/lib/food/structures/server/Menu.ts
@@ -1,7 +1,9 @@
-import got from "got/dist/source";
+import ical from "ical";
+import { DateTime } from "luxon";
 import { ServerDish } from "./Dish";
 import { IMenu, Menu } from "../shared/Menu";
-import API_ENDPOINT from "../../endpoint";
+
+const ICAL_URL = "https://calendar.google.com/calendar/ical/7jpfv1hrcpn08daip5bdlqisvo%40group.calendar.google.com/public/basic.ics";
 
 export default class ServerMenu extends Menu {
   dishes: ServerDish[];
@@ -13,8 +15,38 @@ export default class ServerMenu extends Menu {
   }
 
   public static async fetchAll(): Promise<ServerMenu[]> {
-    const res = await got.get(`${API_ENDPOINT}/menu`).json<IMenu[]>();
+    const ics = await fetch(ICAL_URL).then((res) => res.text());
 
-    return res.map((menu) => new ServerMenu(menu));
+    const data = ical.parseICS(ics);
+
+    const dates = Object.values(data).reduce((accu, ev) => {
+      const dish = ev.summary;
+
+      if (ev.type === "VEVENT" && dish?.length > 0) {
+        const localDate = DateTime.fromJSDate(ev.start).toISODate();
+
+        if (typeof accu[localDate] === "undefined") {
+          // eslint-disable-next-line no-param-reassign
+          accu[localDate] = [];
+        }
+
+        if (accu[localDate].indexOf(dish) < 0) {
+          accu[localDate].push(dish);
+        }
+      }
+
+      return accu;
+    }, {} as Record<string, string[]>);
+
+    return Object
+      .entries(dates)
+      .sort(([a], [b]) => b.localeCompare(a))
+      .map(([date, dishes]) => (new ServerMenu({
+        date,
+        dishes: dishes.map((dish) => ({
+          title: dish,
+          id: dish,
+        })),
+      })));
   }
 }
